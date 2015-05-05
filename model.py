78de5821afd74ce4ad275f1b7bc07f4aca41cc34
Hackbright Student <info@hackbrightacademy.com>
2015-05-04 17:25:51 -0700

    wrote def load_users() function. working. started load_movies() and load_ratings(). need to format datetime for python/sqlalchemy

--- model.py
+++ model.py
@@ -40,7 +40,7 @@ class Movie(db.Model):
 
     movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     title = db.Column(db.String(64), nullable=False)
-    released_at = db.Column(db.DateTime, nullable=True)
+    released_at = db.Column(db.DateTime(), nullable=True)
     imdb_url = db.Column(db.String(200), nullable=True)
 
     def __repr__(self):
---
+++ seed.py
@@ -0,0 +1,68 @@
+"""Utility file to seed ratings database from MovieLens data in seed_data/"""
+
+from model import User, Rating, Movie, connect_to_db, db
+from server import app
+import time
+
+
+def load_users():
+    """Load users from u.user into database."""
+    open_file = open("./seed_data/u.user")
+    
+    for line in open_file:
+        # user_info = line.readline() # creates string of each line, with ending \n character
+        user_info = line.rstrip().split("|") # creates a list of the items on the line
+        user_id, age, zipcode = user_info[0], user_info[1], user_info[-1]
+        new_user = User(user_id=user_id, email="NULL", password="NULL", age=age, zipcode=zipcode)
+        db.session.add(new_user)
+
+    db.session.commit()
+
+
+
+def load_movies():
+    """Load movies from u.item into database."""
+    open_file =open("./seed_data/u.item")
+
+    for line in open_file:
+        # user_info = line.readline() # creates string of each line, with ending \n character
+        movie_info = line.rstrip().split("|") # creates a list of the items on the line
+        movie_id, title, date_old, imdb_url = movie_info[0], movie_info[1], movie_info[2], movie_info[4]
+        title = title[:-7]
+        if date_old:
+            date_old = time.strptime(date_old, "%d-%b-%Y")
+            released_at = time.strftime("%Y-%m-%d", date_old)
+            movie = Movie(movie_id=movie_id, title=title, released_at=released_at, imdb_url=imdb_url)
+        else:
+            movie = Movie(movie_id=movie_id, title=title, imdb_url=imdb_url)
+
+        db.session.add(movie)
+
+    db.session.commit()
+
+# def load_ratings():
+#     """Load ratings from u.data into database."""
+#     open_file = open("./seed_data/u.data")
+    
+#     for line in open_file:
+#         # user_info = line.readline() # creates string of each line, with ending \n character
+#         rating_info = line.rstrip().split("\t") # creates a list of the items on the line
+#         user_id, movie_id, score = rating_info[0], rating_info[1], rating_info[2]
+#         # new_user = User(user_id=user_id, email="NULL", password="NULL", age=age, zipcode=zipcode)
+#         # db.session.add(new_user)
+
+
+#          rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+#     movie_id = db.Column(db.Integer, nullable=False) # relationship("movies")
+#     user_id = db.Column(db.Integer, nullable=False) # relationship("users")
+#     score = 
+
+#     db.session.commit()
+
+if __name__ == "__main__":
+    connect_to_db(app)
+
+    db.create_all()
+    load_users()
+    load_movies()
+    # load_ratings()
