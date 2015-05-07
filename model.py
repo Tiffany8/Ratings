0d708ec8057fc69e871bfb674f5a3757bf03cac7
Hackbright Student <info@hackbrightacademy.com>
2015-05-06 17:30:59 -0700

    user profile page now lists movies and score, movie_list page now list all movies with link to indiv movie page

--- model.py
+++ model.py
@@ -65,6 +65,7 @@ class Rating(db.Model):
     #Define relationship to movie
     movie = db.relationship("Movie", backref=db.backref("ratings", order_by=rating_id))
 
+
     def __repr__(self):
         """Provide helpful representation when printed. """
 
