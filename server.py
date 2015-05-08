32cc0f1653a5b7672021844fa4d8a469c98afc06
Hackbright Student <info@hackbrightacademy.com>
2015-05-07 17:29:40 -0700

    user can rate a new movie, or update their existing rating for a movie.

--- server.py
+++ server.py
@@ -20,19 +20,21 @@ app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
 def index():
-    """Homepage."""
+    """Display the homepage."""
 
     return render_template("homepage.html")
 
 @app.route('/register')
 def registration():
-    """Registration form for new users"""
+    """Display registration form for new users"""
 
     return render_template("register.html")
 
 @app.route('/register', methods=["POST"])
 def register_new_user():
-    """Registration form for new users to GET and then POST"""
+    """When reqistration form is submitted (via POST request), 
+    add them to our User class as a new instance, and add and commit to 'users' DB """
+
     input_email = request.form.get('email')
     input_password = request.form.get('password')
     input_age = request.form.get('age')
@@ -43,7 +45,8 @@ def register_new_user():
     db.session.add(new_user)
     db.session.commit()
 
-    return redirect("/")
+    return redirect("/") # upon successful registration, redirect to homepage
+
 
 @app.route('/users')
 def user_list():
@@ -52,36 +55,61 @@ def user_list():
     users = User.query.all()
     return render_template("user_list.html", users=users)
 
+
 @app.route('/movies')
 def movie_list():
     """Show a list of the movies """
 
     movies_list = Movie.query.all()
-    # rating = Rating.query.first()
-    # rating.movie.title
-    # would be nice to alphabetize movies
+   
     return render_template("movie_list.html", movies_list = movies_list)
 
-@app.route("/movies/<int:id>")
+
+
+@app.route("/movies/<int:id>") # GET request, goes to indiv. movie page
 def get_movie_id(id):
-    movie_object = Movie.query.get(id)
+    """Display movie page by movie_id.
+    Also, based on logged in user, display their rating for that movie, if defined.
+    """
+
+    movie_object = Movie.query.get(id) 
     user_id = session["logged_in_user_id"]
-    # score = request.form.get("")
-
-    # new_rating = Rating(movie_id=movie_object.movie_id, user_id=user_id, score=score)    
-
-    # rating = Rating.query.filter_by(movie.movie_id=Rating.movie_id, 
-                                    # user_id=Rating.user_id)
-
-    # ratings_by_user = Rating.query.filter_by(user_id=user_idcode) 
-  
-    # Class.query.filter_by().first() # this pulls out an instance of a class
 
     return render_template("movie.html", movie_object=movie_object, user_id=user_id)
 
 
+
+@app.route("/movies/<int:id>", methods=["POST"])
+def score_movie(id):
+    """When user submits a new score on the movie page,
+    handle the POST request, and insert the new score into ratings db, 
+    or update the score if it already exists.
+    """
+
+    movie_object = Movie.query.get(id)
+    user_id = session["logged_in_user_id"]
+    score = request.form.get("score")
+
+    
+    ratings_object = Rating.query.filter_by(movie_id = movie_object.movie_id, user_id = user_id).first()
+    # returns a smart object if that user has already scored the movie 
+
+    if ratings_object:
+        ratings_object.score = score # if the score already exists, update it with this newly inputted score
+    else:
+        ratings_object = Rating(movie_id=movie_object.movie_id, user_id=user_id, score=score)  
+        db.session.add(ratings_object)
+        # if no preexisting score is found, and so no smart object, insert a new entry in the ratings table
+
+    db.session.commit()
+    
+    return render_template("movie.html", movie_object=movie_object, user_id=user_id, score=ratings_object.score)
+
+
 @app.route("/users/<int:id>")
 def get_user_by_id(id):
+    """Display user info page by user_id
+    """
     user = User.query.get(id)
 
     return render_template("user.html", user=user)
@@ -99,8 +127,8 @@ def show_login():
 @app.route("/login", methods=["POST"])
 def process_login():
     """Log user into site.
-    Find the user's login credentials located in the 'request.form'
-    dictionary, look up the user, and store them in the session.
+    Compare input email and password to the email and password
+    in the 'users' db for authentification.
     """
 
     input_email = request.form.get('email')
@@ -112,9 +140,8 @@ def process_login():
 
     if user: # if there is a returned value for this user
         
-        if input_password == user.password:
-            session['logged_in_user_id'] = user.user_id
-            # session.setdefault("logged_in_user_id", []).append(user.user_id)
+        if input_password == user.password: 
+            session['logged_in_user_id'] = user.user_id # adding user to the session
 
             flash('Login successful')
             return redirect('/')
@@ -130,12 +157,13 @@ def process_login():
 @app.route("/logout")
 def logout():
     """Logout user"""
+
     del session['logged_in_user_id']
+
     flash("You have been logged out.")
+
     return redirect('/')
     
-
-
 
 
 if __name__ == "__main__":
